48cd84566d4769bd00baa635377fd476aedfbafd
German Eduardo Zapata Segura <gz_junior@hotmail.com>
2022-11-01 15:48:30 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,7 +11,7 @@ var port = Environment.GetEnvironmentVariable("PORT")??"80";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 22042022-v2.0.0 CI/CD in AKS v1.0.0 GermanZapata";
 });
 
 app.MapPost("/", () =>
